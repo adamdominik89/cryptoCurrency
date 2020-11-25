90551597817d3fff3e1ddd4f2bbbdd0e56529ed1
adamdominik89 <adam.dominik89@gmail.com>
2020-11-25 08:30:28 +0100

    Error component code refactoring

--- src/components/pages/Error.vue
+++ src/components/pages/Error.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    Error 404 - page not found please click <router-link :to="{ path: '/' }" >here</router-link> to go to main page
+    Error 404 - page not found please click
+    <router-link :to="{ path: '/' }">here</router-link>
+    to go to main page
   </div>
 
 </template>

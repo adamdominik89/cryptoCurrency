39aff4fc2d04798bb434c35bc763fa7fc3561d0c
adamdominik89 <adam.dominik89@gmail.com>
2020-11-25 08:29:04 +0100

    page not found error handling

---
+++ src/components/pages/Error.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Error 404 - page not found please click <router-link :to="{ path: '/' }" >here</router-link> to go to main page
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'ErrorPage'
+}
+
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,21 +2,30 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import CurrenciesList from '../components/pages/Currencies-list'
+import ErrorPage from '../components/pages/Error'
 
-
+const routes = [
+    {
+        path: '/',
+        name: 'CurrenciesList',
+        component: CurrenciesList
+    },
+    {
+        path: '/errorPage',
+        name: 'ErrorPage',
+        component: ErrorPage
+    }
+]
 Vue.use(Router)
 
 const router = new Router({
-    routes: [
-        {
-            path: '/',
-            name: 'CurrenciesList',
-            component: CurrenciesList
-        }
-    ]
+    routes
 })
 router.beforeEach((to, from, next) => {
-
-    next()
+    if ((routes.find(route => route.path === to.path) && to.path !== '/errorPage') || to.path === '/errorPage') {
+        next()
+    } else {
+        next({name: 'ErrorPage'})
+    }
 })
 export default router
